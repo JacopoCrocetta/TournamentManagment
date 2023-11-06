ff24d5d660ef0aed42e2b3ed57785fd77f456204
Jacopo <jacopo.crocetta@urbanasmart.com>
2023-11-06 17:18:01 +0100

    added some stuff on prisma and added prisma on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ControllerModule } from "./infrastructure/controller/controller.module";
 import { AppController } from "./infrastructure/controller/healtz.controller";
+import { PrismaService } from "./infrastructure/database/prisma.service";
 
 @Module({
     imports:[ControllerModule],
-    controllers:[AppController]
+    controllers:[AppController], 
+    providers: [PrismaService]
 })
 export class AppModule{}
--- src/infrastructure/database/prisma.service.ts
+++ src/infrastructure/database/prisma.service.ts
@@ -1,14 +1,16 @@
-import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
+import { INestApplication, Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit{
     async onModuleInit() {
+        Logger.log("CONNECTING...");
         await this.$connect();
     }
 
     async enableShutdownHooks(app: INestApplication) {
         this.$on('beforeExit', async () => {
+            Logger.log("CLOSING APP...");
             await app.close();
         });
     }
