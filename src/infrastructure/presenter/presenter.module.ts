84b7c871a808b70c81e75e7b7eed0b2487578b99
Jacopo <jacopo.crocetta@urbanasmart.com>
2024-01-08 16:58:45 +0100

    Added crud for tournament

--- src/infrastructure/presenter/presenter.module.ts
+++ src/infrastructure/presenter/presenter.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TournamentPagePresenter } from "./tournament/page/tournament-page.presenter";
 import { UseCasesModule } from "src/core/use-case/use-cases.module";
+import { TournamentUpdatePresenter } from "./tournament/update/tournament-update";
+import { TournamentCreatePresenter } from "./tournament/create/tournament-create.presenter";
+import { TournamentDeletePresenter } from "./tournament/delete/tournament-delete.presenter";
 
-const providers = [TournamentPagePresenter, UseCasesModule];
+const providers = [TournamentPagePresenter, TournamentCreatePresenter, TournamentDeletePresenter, TournamentUpdatePresenter, UseCasesModule];
 @Module({
   imports:[UseCasesModule],
   providers,
