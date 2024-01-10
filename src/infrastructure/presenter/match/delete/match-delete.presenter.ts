efdf4b7be9ead936f042a7200b879a2bc97002d5
Jacopo <jacopo.crocetta@urbanasmart.com>
2024-01-10 17:33:55 +0100

    fixed naming

--- src/infrastructure/presenter/match/delete/match-delete.presenter.ts
+++ src/infrastructure/presenter/match/delete/match-delete.presenter.ts
@@ -4,7 +4,7 @@ import { IMatchRepository } from "src/core/repository/match.repository";
 export class MatchDeletePresenter {
     constructor(private repository: IMatchRepository){}
 
-    async deleteTournamentById(id: number): Promise<boolean>{
+    async deleteMatchById(id: number): Promise<boolean>{
         Logger.log("TRYING TO DELETE THE MATCH WITH ID " + id);
         return await this.repository.delete(id);
     }
