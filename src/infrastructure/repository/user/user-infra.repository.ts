ecf4f02cd60f699705034cbdf81b0e88ce19de32
Jacopo <jacopo.crocetta@urbanasmart.com>
2024-01-12 12:58:31 +0100

    User repo

--- src/infrastructure/repository/user/user-infra.repository.ts
+++ src/infrastructure/repository/user/user-infra.repository.ts
@@ -1,4 +1,22 @@
 import { PrismaService } from "src/infrastructure/database/prisma.service";
+import { Prisma } from "@prisma/client";
+
+type userCreateInput = Prisma.UsersCreateInput;
+type WhereUniqueInput = Prisma.UsersWhereUniqueInput;
+
+export interface UpdateParams {
+    where: Prisma.UsersWhereUniqueInput;
+    data: Prisma.UsersUpdateInput;
+  }
+  
+  export interface GetManyParams {
+    skip: number;
+    take: number;
+    cursor: Prisma.TournamentsWhereUniqueInput;
+    where: Prisma.TournamentsWhereInput;
+    orderBy: Array<Prisma.TournamentsOrderByWithRelationInput>;
+    include: Prisma.TournamentsInclude
+  }
 
 export abstract class UserInfrastructureRepository  {
     constructor(protected prismaService:PrismaService){}
