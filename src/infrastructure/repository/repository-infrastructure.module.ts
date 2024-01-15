440853c1d5ded516a23472e25619abac1d867030
Jacopo <jacopo.crocetta@urbanasmart.com>
2024-01-15 15:10:40 +0100

    rollback

--- src/infrastructure/repository/repository-infrastructure.module.ts
+++ src/infrastructure/repository/repository-infrastructure.module.ts
@@ -1,10 +1,5 @@
 import { Module } from "@nestjs/common";
-import { TournamentsInfrastructureRepository } from "./tournament/tournaments-infra.repository";
-
-export const providers = [TournamentsInfrastructureRepository];
 
 @Module({
-    providers:[...providers],
-  exports: [...providers],
 })
 export class RepositoryInfrastructureModule {}
